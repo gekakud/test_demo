4dbbf44da4206c0b8d744caa6f321d6a695850cd
Evgeny Pekerman <geka.kud@gmail.com>
2022-11-08 18:05:10 +0200

    camera_stream

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-from pages.camera_filter import app_video_filters
 from pages.camera_stream import app_loopback
-from pages.code_edit import code_edit
-from pages.upload_files import upload_files
-from pages.image_preview import preview
+
 
 def main():
     st.sidebar.image("logo.png", use_column_width=True)
@@ -15,14 +12,6 @@ def main():
                         icons=['file-earmark-code', 'check2-circle', 'cpu-fill'], menu_icon="cast", default_index=0)
     if s == "Video stream":
         app_loopback()
-    elif s == "Image preview":
-        preview()
-    elif s == "Video filter":
-        app_video_filters()
-    elif s == "Code editor":
-        code_edit()
-    else:
-        upload_files()
 
 if __name__ == "__main__":
     main()
