7e733baf11c144884711653da8bb975dfdb8c104
Evgeny Pekerman <geka.kud@gmail.com>
2022-11-08 18:40:27 +0200

    Update camera_stream.py

--- pages/camera_stream.py
+++ pages/camera_stream.py
@@ -3,5 +3,5 @@ from streamlit_webrtc import webrtc_streamer
 
 def app_loopback():
     """Simple video loopback"""
-    webrtc_streamer(key="loopback")
+    webrtc_streamer(key="loopback", rtc_configuration={"iceServers":[{"urls":["stun:stun.l.google.com:19302"]}]})
     
